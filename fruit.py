50993ab3da01b7bb2436605bc912e1ad64c38a7a
noam emilio radetich <noam81@yahoo.it>
2025-09-17 15:12:12 +0200

    added fruit

--- fruit.py
+++ fruit.py
@@ -2,17 +2,17 @@
 import pygame
 
 class Fruit(pygame.sprite.Sprite):
-    def __init__(self, groups: pygame.sprite.Sprite, x: int, y: int, screen: pygame.display, size: int, speed: int):
+    def __init__(self, groups: pygame.sprite.Sprite, x: int, y: int, size: int, speed: int,color:tuple):
         super().__init__(groups)
         self.x = x
         self.y = y
-        self.screen = screen
         self.size = size
         self.fruit = True
         self.image = pygame.Surface((size, size))
         self.rect = pygame.Rect(x, y, size, size)
         self.speed = speed
-        self.groups = groups
+        self.color = color
+        self.image.fill(self.color)
 
     def update_position(self, x: int, y: int):
         self.x = x
